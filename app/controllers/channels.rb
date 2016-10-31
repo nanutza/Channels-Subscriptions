8e6e670baaf9e8b06f7f5c4ca8713cc2193a608f
Nanutza <nanutza@gmail.com>
2016-10-31 02:46:41 -0400

    User Profile page with list of channels

--- app/controllers/channels.rb
+++ app/controllers/channels.rb
@@ -2,3 +2,21 @@ get '/channels'  do
 	@channels = Channel.all
 	erb :'channels/index'
 end
+
+get '/channels/:id' do
+  @channel = Channel.find_by(id: params[:id])
+  subscribers = @channel.subscribers
+  subscribers.each do |subscriber|
+    if current_user && subscriber.id == current_user.id
+      @subscribed = true
+    else
+      @subscribed = false
+    end
+  end
+  erb :'channels/show'
+end
+
+post '/channels/:id' do
+    erb :'404'
+  # redirect "/channels/#{channel.id}"
+end
